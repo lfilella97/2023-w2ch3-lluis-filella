89b386ce02a2b498dc7884e21bdbec29893972c9
lfilella97 <lfilella97@gmail.com>
2023-01-18 21:17:08 +0100

    add changes made by eslint

--- precurse-projects/calculadora/Calculadora PRO.js
+++ precurse-projects/calculadora/Calculadora PRO.js
@@ -1,93 +1,137 @@
+let valoresIntroducidos;
+let result;
+
 const calculadora = () => {
-    let resultado = []
-    do{
-        getNumbers()
-        console.log(valoresIntroducidos)
-        if(result != "0" && result != "" && result != 0){
-            resultado.unshift(result)
-        }
-        resultado != 0 ? alert('Ultimos resultados:' + resultado) : resultado
-        response = confirm(`Quieres hacer otro calculo? si -> aceptar / no -> cancelar`)
-    }while(response != false)
-    alert('Bye!')
-}
+  let response;
+  const resultado = [];
+  do {
+    getNumbers();
+    console.log(valoresIntroducidos);
+    if (result !== "0" && result !== "" && result !== 0) {
+      resultado.unshift(result);
+    }
+
+    if (resultado !== 0) {
+      alert("Ultimos resultados:" + resultado);
+    }
+
+    response = confirm(
+      `Quieres hacer otro calculo? si -> aceptar / no -> cancelar`
+    );
+  } while (response !== false);
+
+  alert("Bye!");
+};
+
 const getNumbers = () => {
-    valoresIntroducidos = []
-    result = []
-    let = count = -1;
-    do{
-        valoresIntroducidos.push(prompt(`Introduce aqui tu numero: (para dejar de introducir numeros pulsa Cancelar)`));
-        count++;
-        if (valoresIntroducidos[count] == ""){
-            alert('ERROR: No has introducido ningun valor');
-            break;
-        }else if((valoresIntroducidos[count] == (null ))){
-                valoresIntroducidos.pop();
-            if(valoresIntroducidos.length == 1){
-                result.push('\nEl cuadrado de tu numero es: ' + valoresIntroducidos[0]* valoresIntroducidos[0])
-            }else if(valoresIntroducidos.length == 0){
-                alert('ERROR: No has introducido ningun valor');
-                break;
-            }else{
-                result.push(operaciones())
-            }
-                break;
-        }else if(isNaN(parseFloat(valoresIntroducidos[count]))){
-            alert('ERROR: has introducido un valor invalido');
-            break;
-        } 
-    }while (count < 100);
-    return result;
-}
+  valoresIntroducidos = [];
+  result = [];
+  let count = -1;
+  do {
+    valoresIntroducidos.push(
+      prompt(
+        `Introduce aqui tu numero: (para dejar de introducir numeros pulsa Cancelar)`
+      )
+    );
+    count++;
+    if (valoresIntroducidos[count] === "") {
+      alert("ERROR: No has introducido ningun valor");
+      break;
+    } else if (valoresIntroducidos[count] === null) {
+      valoresIntroducidos.pop();
+      if (valoresIntroducidos.length === 1) {
+        result.push(
+          "\nEl cuadrado de tu numero es: " +
+            valoresIntroducidos[0] * valoresIntroducidos[0]
+        );
+      } else if (valoresIntroducidos.length === 0) {
+        alert("ERROR: No has introducido ningun valor");
+        break;
+      } else {
+        result.push(operaciones());
+      }
+
+      break;
+    } else if (isNaN(parseFloat(valoresIntroducidos[count]))) {
+      alert("ERROR: has introducido un valor invalido");
+      break;
+    }
+  } while (count < 100);
+
+  return result;
+};
+
 const operaciones = () => {
-    const sum = () => {
-        let acc = 0;
-        for (num in valoresIntroducidos) {
-            (num == 0)? 
-            acc = parseFloat(valoresIntroducidos[num]) :
-            acc += parseFloat(valoresIntroducidos[num]);
-        }
-        if (!(Number.isInteger(acc))){
-            acc = acc.toFixed(3)
-        }
-        return parseFloat(acc);
+  const sum = () => {
+    let acc = 0;
+    for (const num in valoresIntroducidos) {
+      num == 0
+        ? (acc = parseFloat(valoresIntroducidos[num]))
+        : (acc += parseFloat(valoresIntroducidos[num]));
     }
-    const res = () => {
-        let acc = 0;
-        for (num in valoresIntroducidos) {
-            (num == 0)? 
-            acc = parseFloat(valoresIntroducidos[num]) :
-            acc -= parseFloat(valoresIntroducidos[num]);
-        }
-        if (!(Number.isInteger(acc))){
-            acc = acc.toFixed(3)
-        }
-        return parseFloat(acc);
+
+    if (!Number.isInteger(acc)) {
+      acc = acc.toFixed(3);
     }
-    const mult = () => {
-        let acc;
-        for (num in valoresIntroducidos) {
-            (num == 0)? 
-            acc = parseFloat(valoresIntroducidos[num]) :
-            acc *= parseFloat(valoresIntroducidos[num]);
-        }
-        if (!(Number.isInteger(acc))){
-            acc = acc.toFixed(3)
-        }
-        return parseFloat(acc);
+
+    return parseFloat(acc);
+  };
+
+  const res = () => {
+    let acc = 0;
+    for (num in valoresIntroducidos) {
+      num == 0
+        ? (acc = parseFloat(valoresIntroducidos[num]))
+        : (acc -= parseFloat(valoresIntroducidos[num]));
     }
-    const div = () => {
-        let acc = 0;
-        for (num in valoresIntroducidos) {
-            (num == 0)? 
-            acc = parseFloat(valoresIntroducidos[num]) :
-            acc /= parseFloat(valoresIntroducidos[num]);
-        }
-        if (!(Number.isInteger(acc))){
-            acc = acc.toFixed(3)
-        }
-        return parseFloat(acc);
+
+    if (!Number.isInteger(acc)) {
+      acc = acc.toFixed(3);
     }
-    return '\nEl resultado de las operaciones es: ' + sum() + ', ' + res() + ', ' + mult() + ', ' +  div()
-}
-calculadora()
+
+    return parseFloat(acc);
+  };
+
+  const mult = () => {
+    let acc;
+    for (num in valoresIntroducidos) {
+      num == 0
+        ? (acc = parseFloat(valoresIntroducidos[num]))
+        : (acc *= parseFloat(valoresIntroducidos[num]));
+    }
+
+    if (!Number.isInteger(acc)) {
+      acc = acc.toFixed(3);
+    }
+
+    return parseFloat(acc);
+  };
+
+  const div = () => {
+    let acc = 0;
+    for (num in valoresIntroducidos) {
+      num == 0
+        ? (acc = parseFloat(valoresIntroducidos[num]))
+        : (acc /= parseFloat(valoresIntroducidos[num]));
+    }
+
+    if (!Number.isInteger(acc)) {
+      acc = acc.toFixed(3);
+    }
+
+    return parseFloat(acc);
+  };
+
+  return (
+    "\nEl resultado de las operaciones es: " +
+    sum() +
+    ", " +
+    res() +
+    ", " +
+    mult() +
+    ", " +
+    div()
+  );
+};
+
+calculadora();
